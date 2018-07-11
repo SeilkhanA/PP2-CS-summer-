51b68bd81f38474c6e8f52d1792ebfc20afe41ae
SeilkhanA <40297551+SeilkhanA@users.noreply.github.com>
2018-07-11 10:03:13 +0600

    Add files via upload

---
+++ Calculator with Delegate/Calculator with Delegate/Brain.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_with_Delegate
+{
+    public enum WhatOperation
+    {
+        Plus,
+        Minus,
+        Divide,
+        Multiply,
+        None
+    }
+
+    public enum State
+    {
+        Zero,
+        AccumulateDigits,
+        Compute,
+        Result
+    }
+
+    public class Brain
+    {
+        MyDelegate d;
+
+        string text;
+
+        string first;
+        string second;
+
+        WhatOperation operation = WhatOperation.None;
+        State state = State.Zero;
+
+
+        public Brain(MyDelegate d)
+        {
+            this.d = d;
+        }
+
+        public void Process(string command)
+        {
+            switch (state)
+            {
+                case State.Zero:
+                    Zero(false, command);
+                    break;
+                case State.AccumulateDigits:
+                    AccumulateDigits(false, command);
+                    break;
+                case State.Compute:
+                    Compute(false, command);
+                    break;
+                case State.Result:
+                    Result(false, command);
+                    break;
+            }
+
+        }
+
+        void Zero(bool input, string command)
+        {
+            if (input)
+            {
+                d.Invoke("0");
+            }
+            else
+            {
+                if (command.Length == 1 && command[0] <= '9' && command[0] > '0')
+                {
+                    AccumulateDigits(true, command);
+                }
+                else
+                {
+                    Zero(true, command);
+                }
+            }
+        }
+
+        void AccumulateDigits(bool input, string command)
+        {
+            if (input)
+            {
+                state = State.AccumulateDigits;
+                text = text + command;
+                d.Invoke(text);
+            }
+            else                                                  // состояние false 
+            {
+                if (command.Length == 1 && command[0] == '+')
+                {
+                    Compute(true, command);
+                    operation = WhatOperation.Plus;
+                }
+                else if(command.Length == 1 && command[0] == '-')
+                {
+                    Compute(true, command);
+                    operation = WhatOperation.Minus;
+                }
+                else if (command.Length == 1 && command[0] == '×')
+                {
+                    Compute(true, command);
+                    operation = WhatOperation.Multiply;
+                }
+                else if (command.Length == 1 && command[0] == '/')
+                {
+                    Compute(true, command);
+                    operation = WhatOperation.Divide;
+                }
+
+                
+                else if (command.Length == 1 && command[0] <= '9' && command[0] >= '0')
+                {
+                    AccumulateDigits(true, command);
+                }
+
+                else if (command.Length == 1 && command[0] == '=')
+                {
+                    Result(true, command);
+                }
+            }
+        }
+
+        void Compute(bool input, string command)
+        {
+            if (input)
+            {
+                state = State.Compute;
+                first = text;
+                text = "";
+            }
+            else
+            {
+                if (command.Length == 1 && command[0] <= '9' && command[0] >= '0')
+                {
+                    AccumulateDigits(true, command);
+                }
+            }
+        }
+
+        void Result(bool input, string command)
+        {
+            if (input)
+            {
+                state = State.Result;
+                second = text;
+                switch (operation)
+                {
+
+                    case WhatOperation.Plus:
+                        text = (int.Parse(first) + int.Parse(second)).ToString();
+                        break;
+
+                    case WhatOperation.Minus:
+                        text = (int.Parse(first) - int.Parse(second)).ToString();
+                        break;
+
+                    case WhatOperation.Multiply:
+                        text = (int.Parse(first) * int.Parse(second)).ToString();
+                        break;
+
+                    case WhatOperation.Divide:
+                        text = (int.Parse(first) / int.Parse(second)).ToString();
+                        break;
+                    case WhatOperation.None:
+                        text = "MATH ERROR!";
+                        break;
+                    
+                    default:
+                        break;
+                }
+
+                d.Invoke(text);
+                text = "";
+            }
+            else if (command.Length == 1 && command[0] <= '9' && command[0] >= '0')
+            {
+                AccumulateDigits(true, command);
+            }
+            else
+            {
+                AccumulateDigits(false, command);
+                
+            }
+        }
+    }
+}
+//else if (command.Length == 1 && command[0] == '%')
+//{
+//    Compute(true, command);
+//    operation = WhatOperation.Percent;
+//}
+//else if (command.Length == 1 && command[0] == '²')
+//{
+//    Compute(true, command);
+//    operation = WhatOperation.Sqrd;
+//}
+
+//else if (command.Length == 1 && command[0] == '√')
+//{
+//    Compute(true, command);
+//    operation = WhatOperation.Sqrt;
+//}
+
+
+//case WhatOperation.Percent:
+//    text = (int.Parse(first) / 100).ToString();
+//    break;
+//case WhatOperation.Sqrd:
+//    text = (int.Parse(first) * int.Parse(first)).ToString();
+//    break;
+//case WhatOperation.Sqrt:
+//    text = (int.Parse(first) / int.Parse(second)).ToString();
+//    break;
---
+++ Calculator with Delegate/Calculator with Delegate/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_with_Delegate
+{
+    public delegate void MyDelegate(string text);
+
+    public partial class Form1 : Form
+    {
+        Brain b;
+
+        public Form1()
+        {
+            InitializeComponent();
+            b = new Brain(DisplayResult);
+        }
+
+        public void DisplayResult(string text)
+        {
+            result.Text = text;
+        }
+
+        public void BtnPressed(object sender, EventArgs e)
+        {
+            b.Process((sender as Button).Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void button16_Click(object sender, EventArgs e)
+        //{
+        //    result.Clear();
+        //}
+    }
+}
